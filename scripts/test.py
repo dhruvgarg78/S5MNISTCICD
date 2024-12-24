ddfe2ff769cfee13fa24dd8cae5ed4db24721677
Dhruv Garg <dhruvg7864@gmail.com>
2024-12-24 14:18:22 -0600

    Fixed module import issue in test.py

--- scripts/test.py
+++ scripts/test.py
@@ -1,4 +1,7 @@
 import torch
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from model import UltraSmallCNN
 from scripts.train import train_model  # Import the train function
 
